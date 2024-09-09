5595ed7f2a81c40c984357170ebf4cdcf159bf1d
Max Sobol Mark <maxsobolmark@gmail.com>
2024-09-09 18:06:15 -0400

    Optimization for maze2d scripted policy

--- d4rl/pointmaze/waypoint_controller.py
+++ d4rl/pointmaze/waypoint_controller.py
@@ -9,7 +9,14 @@ ONES = np.zeros((2,), dtype=np.float32)
 
 
 class WaypointController(object):
-    def __init__(self, maze_str, solve_thresh=0.1, p_gain=10.0, d_gain=-1.0):
+    def __init__(
+        self,
+        maze_str,
+        solve_thresh=0.1,
+        p_gain=10.0,
+        d_gain=-1.0,
+        target_to_q_values={},
+    ):
         self.maze_str = maze_str
         self._target = -1000 * ONES
 
@@ -24,12 +31,14 @@ class WaypointController(object):
 
         self.env = grid_env.GridEnv(grid_spec.spec_from_string(maze_str))
 
+        self.target_to_q_values = target_to_q_values
+
     def current_waypoint(self):
         return self._waypoints[self._waypoint_idx]
 
     def get_action(self, location, velocity, target):
-        if np.linalg.norm(self._target - np.array(self.gridify_state(target))) > 1e-3: 
-            #print('New target!', target, 'old:', self._target)
+        if np.linalg.norm(self._target - np.array(self.gridify_state(target))) > 1e-3:
+            # print('New target!', target, 'old:', self._target)
             self._new_target(location, target)
 
         dist = np.linalg.norm(location - self._target)
@@ -47,10 +56,17 @@ class WaypointController(object):
         action = self.p_gain * prop + self.d_gain * velocity
 
         dist_next_wpnt = np.linalg.norm(location - next_wpnt)
-        if task_not_solved and (dist_next_wpnt < self.solve_thresh) and (vel_norm<self.vel_thresh):
+        if (
+            task_not_solved
+            and (dist_next_wpnt < self.solve_thresh)
+            and (vel_norm < self.vel_thresh)
+        ):
             self._waypoint_idx += 1
-            if self._waypoint_idx == len(self._waypoints)-1:
-                assert np.linalg.norm(self._waypoints[self._waypoint_idx] - self._target) <= self.solve_thresh
+            if self._waypoint_idx == len(self._waypoints) - 1:
+                assert (
+                    np.linalg.norm(self._waypoints[self._waypoint_idx] - self._target)
+                    <= self.solve_thresh
+                )
 
         self._waypoint_prev_loc = location
         action = np.clip(action, -1.0, 1.0)
@@ -60,7 +76,7 @@ class WaypointController(object):
         return (int(round(state[0])), int(round(state[1])))
 
     def _new_target(self, start, target):
-        #print('Computing waypoints from %s to %s' % (start, target))
+        # print('Computing waypoints from %s to %s' % (start, target))
         start = self.gridify_state(start)
         start_idx = self.env.gs.xy_to_idx(start)
         target = self.gridify_state(target)
@@ -68,7 +84,11 @@ class WaypointController(object):
         self._waypoint_idx = 0
 
         self.env.gs[target] = grid_spec.REWARD
-        q_values = q_iteration.q_iteration(env=self.env, num_itrs=50, discount=0.99)
+        if target in self.target_to_q_values:
+            q_values = self.target_to_q_values[target]
+        else:
+            q_values = q_iteration.q_iteration(env=self.env, num_itrs=50, discount=0.99)
+            self.target_to_q_values[target] = q_values
         # compute waypoints by performing a rollout in the grid
         max_ts = 100
         s = start_idx
@@ -79,7 +99,7 @@ class WaypointController(object):
 
             waypoint = self.env.gs.idx_to_xy(new_s)
             if new_s != target_idx:
-                waypoint = waypoint - np.random.uniform(size=(2,))*0.2
+                waypoint = waypoint - np.random.uniform(size=(2,)) * 0.2
             waypoints.append(waypoint)
             s = new_s
             if new_s == target_idx:
@@ -92,18 +112,14 @@ class WaypointController(object):
 
 if __name__ == "__main__":
     print(q_iteration.__file__)
-    TEST_MAZE = \
-            "######\\"+\
-            "#OOOO#\\"+\
-            "#O##O#\\"+\
-            "#OOOO#\\"+\
-            "######"
+    TEST_MAZE = "######\\" + "#OOOO#\\" + "#O##O#\\" + "#OOOO#\\" + "######"
     controller = WaypointController(TEST_MAZE)
-    start = np.array((1,1), dtype=np.float32)
-    target = np.array((4,3), dtype=np.float32)
+    start = np.array((1, 1), dtype=np.float32)
+    target = np.array((4, 3), dtype=np.float32)
     act, done = controller.get_action(start, target)
-    print('wpt:', controller._waypoints)
+    print("wpt:", controller._waypoints)
     print(act, done)
-    import pdb; pdb.set_trace()
-    pass
+    import pdb
 
+    pdb.set_trace()
+    pass
